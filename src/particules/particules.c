ac1ecdd70528fb29a8b469aabd9ab8bd3b6f64b2
felix barthes <fbarthes@localhost.localdomain>
2018-05-02 15:03:34 +0200

    particule ok ,no norme

--- src/particules/particules.c
+++ src/particules/particules.c
@@ -1,93 +1,97 @@
 /*
 ** EPITECH PROJECT, 2018
-** my_rpg
+** particuleset
 ** File description:
-** particules.c
+** particuleset.c
 */
 
-#include "particules.h"
+#include"particules.h"
+#include"my_rpg.h"
 
-void update_part_buffer_set(partBuffer *this, uint id)
+void updatepartbufferset(partBuffer *this,uint id)
 {
-	this->vertex[(id * 4) + 0].position.x += this->info[id].speed.x;
-	this->vertex[(id * 4) + 1].position.x += this->info[id].speed.x;
-	this->vertex[(id * 4) + 2].position.x += this->info[id].speed.x;
-	this->vertex[(id * 4) + 3].position.x += this->info[id].speed.x;
-	this->vertex[(id * 4) + 0].position.y += this->info[id].speed.y;
-	this->vertex[(id * 4) + 1].position.y += this->info[id].speed.y;
-	this->vertex[(id * 4) + 2].position.y += this->info[id].speed.y;
-	this->vertex[(id * 4) + 3].position.y += this->info[id].speed.y;
-	this->vertex[(id * 4) + 0].color.a
-	= (int)(this->info[id].life * 255);
-	this->vertex[(id * 4) + 1].
-	color.a = (int)(this ->info[id].life * 255);
-	this->vertex[(id * 4) + 2].
-	color.a = (int)(this ->info[id].life * 255);
-	this->vertex[(id * 4) + 3].
-	color.a = (int)(this ->info[id].life * 255);
+	this ->vertex [(id * 4) + 0]. position.x += this->info[id].speed.x;
+	this ->vertex [(id * 4) + 1]. position.x += this->info[id].speed.x;
+	this ->vertex [(id * 4) + 2]. position.x += this->info[id].speed.x;
+	this ->vertex [(id * 4) + 3]. position.x += this->info[id].speed.x;
+	this ->vertex [(id * 4) + 0]. position.y += this->info[id].speed.y;
+	this ->vertex [(id * 4) + 1]. position.y += this->info[id].speed.y;
+	this ->vertex [(id * 4) + 2]. position.y += this->info[id].speed.y;
+	this ->vertex [(id * 4) + 3]. position.y += this->info[id].speed.y;
+	this ->vertex [(id * 4) + 0]. color.a
+	= (int)(this ->info[id].life * 255.);
+	this ->vertex [(id * 4) + 1].
+	color.a = (int)(this ->info[id].life * 255.);
+	this ->vertex [(id * 4) + 2].
+	color.a = (int)(this ->info[id].life * 255.);
+	this ->vertex [(id * 4) + 3].
+	color.a = (int)(this ->info[id].life * 255.);
 
 }
-
-void draw_part_buffer(partBuffer *this, sfRenderWindow *window)
+void drawPartBufer(partBuffer *this , sfRenderWindow *window)
 {
-	sfRenderWindow_drawPrimitives(window, this->vertex,
-	this->size * 4, sfQuads, NULL);
+	sfRenderWindow_drawPrimitives(window , this ->vertex
+	, this ->size * 4, sfQuads , NULL);
 }
 
-void set_part_v1(partBuffer *this, uint id, sfVector2f pos)
+void setPartv1(partBuffer *this , uint id , sfVector2f  pos)
 {
-	if (id >= this->size)
+	if (id  >= this ->size)
 		return;
-	this->vertex[(id * 4) + 0].position
-		= (sfVector2f){pos.x + 0, pos.y + 0};
-	this->vertex[(id * 4) + 1].position
-		= (sfVector2f){pos.x + 45, pos.y + 0};
-	this->vertex[(id * 4) + 2].position
-		= (sfVector2f){pos.x + 45, pos.y + 45};
-	this->vertex[(id * 4) + 3].position
-		= (sfVector2f){pos.x + 0, pos.y + 45};
-	this->vertex[(id * 4) + 0].color = sfRed;
-	this->vertex[(id * 4) + 1].color = sfMagenta;
-	this->vertex[(id * 4) + 2].color = sfBlack;
-	this->vertex[(id * 4) + 3].color = sfRed;
-	this->info[id].speed.x = (((float)(rand() % 255) / 255) - 0.5) * 0.1;
-	this->info[id].speed.y = (((float)(rand() % 255) / 255) - 0.5) * 0.1;
-	this->info[id].life = 1;
+	this ->vertex [(id * 4) + 0]. position
+		= (sfVector2f){pos.x + 0 , pos.y + 0};
+	this ->vertex [(id * 4) + 1]. position
+		= (sfVector2f){pos.x + 10, pos.y + 0};
+	this ->vertex [(id * 4) + 2]. position
+		= (sfVector2f){pos.x + 10, pos.y + 10};
+	this ->vertex [(id * 4) + 3]. position
+		= (sfVector2f){pos.x + 0, pos.y + 10};
+	this ->vertex [(id * 4) + 0]. color = sfRed;
+	this ->vertex [(id * 4) + 1]. color = sfMagenta;
+	this ->vertex [(id * 4) + 2]. color = sfBlack;
+	this ->vertex [(id * 4) + 3]. color = sfRed;
+	this ->info[id]. speed.x = ((( float)(rand() % 255) / 255) - 0.5) *0.1;
+	this ->info[id]. speed.y = ((( float)(rand() % 255) / 255) - 0.5) *0.1;
+	this ->info[id].life = 1;
 }
 
-void set_part_v2(partBuffer *this, uint id, sfVector2f pos, sfColor color)
+void setPartv2(partBuffer *this , uint id , sfVector2f  pos, sfColor  color)
 {
-	if (id >= this->size)
+	if (id  >= this ->size)
 		return ;
-	this->vertex[(id * 4) + 0].position =
-		(sfVector2f){pos.x + 0, pos.y + 0};
-	this->vertex[(id * 4) + 1].position =
-		(sfVector2f){pos.x + 45, pos.y + 0};
-	this->vertex[(id * 4) + 2].position =
-		(sfVector2f){pos.x + 45, pos.y + 45};
-	this->vertex[(id * 4) + 3].position =
-		(sfVector2f){pos.x + 0, pos.y + 45};
-	this->vertex[(id * 4) + 0].color = color;
-	this->vertex[(id * 4) + 1].color = color;
-	this->vertex[(id * 4) + 2].color = color;
-	this->vertex[(id * 4) + 3].color = color;
-	this->info[id].speed.x = (((float)(rand() % 255) / 255) - 0.5) * 0.1;
-	this->info[id].speed.y = (((float)(rand() % 255) / 255) - 0.5) * 0.1;
-	this->info[id].life = 1;
+	this ->vertex [(id * 4) + 0]. position =
+		(sfVector2f){pos.x + 0 , pos.y + 0};
+	this ->vertex [(id * 4) + 1]. position =
+		(sfVector2f){pos.x + 5, pos.y + 0};
+	this ->vertex [(id * 4) + 2]. position =
+		(sfVector2f){pos.x + 5, pos.y + 5};
+	this ->vertex [(id * 4) + 3]. position =
+		(sfVector2f){pos.x + 0 , pos.y + 5};
+	this ->vertex [(id * 4) + 0]. color = color;
+	this ->vertex [(id * 4) + 1]. color = color;
+	this ->vertex [(id * 4) + 2]. color = color;
+	this ->vertex [(id * 4) + 3]. color = color;
+	this ->info[id]. speed.x = ((( float)(rand() % 255) / 255) - 0.5) *0.1;
+	this ->info[id]. speed.y = ((( float)(rand() % 255) / 255) - 0.5) *0.1;
+	this ->info[id].life = 1;
 }
 
-void update_part_buffer(partBuffer *this)
+void updatePartBuffer(partBuffer *this)
 {
-	for (uint id = 0; id < this->size; id += 1) {
-		if (this->info[id].life <= 0)
+	for (uint id = 0; id < this ->size; id += 1)
+	{
+		if (this ->info[id].life  <= 0)
 			continue;
-		this->info[id].life -= 1 / 120;
-		if (this->info[id].life <= 0) {
+		this ->info[id].life  -= 1/120.;
+		if (this ->info[id].life  <= 0)
+		{
 			memset(this ->vertex +
 			(id * 4), 0, sizeof(sfVertex) * 4);
 			continue;
 		}
-		for (uint id = 0; id < this->size; id += 1)
-			update_part_buffer_set(this, id);
+		for (uint id = 0; id < this ->size; id += 1)
+		{
+			updatepartbufferset(this,id);
+		}
 	}
 }
--- src/particules/particules_init.c
+++ src/particules/particules_init.c
@@ -1,86 +1,80 @@
 /*
 ** EPITECH PROJECT, 2018
-** my_rpg
+** particuleinit
 ** File description:
-** particules_init.c
+** particuleinit.c
 */
 
-#include "particules.h"
+#include"my_rpg.h"
+#include"particules.h"
 
-partBuffer *new_part_buffer(int size)
+partBuffer *newPartBuffer(int  size)
 {
-	partBuffer *this;
-	const size_t size_m = (sizeof(partBuffer) +
-	sizeof(sfVertex) * size * 4 +
-	sizeof(t_info) * size);
+	partBuffer     *this;
+	const  size_t   size_m = (sizeof(partBuffer) +
+				  sizeof(sfVertex) * size * 4 +
+				  sizeof(t_info) * size);
 	void *ptn = malloc(size_m);
-
 	if (ptn == NULL)
 		return (NULL);
-	memset(ptn, 0, size_m);
+	memset(ptn , 0, size_m);
 	this = (partBuffer *)(ptn);
-	this->size = size;
-	this->vertex = (sfVertex *)(ptn + sizeof(partBuffer));
-	this->info = (t_info *)(this->vertex + (size * 4));
+	this ->size = size;
+	this ->vertex = (sfVertex *)(ptn + sizeof(partBuffer));
+	this ->info = (t_info *)(this ->vertex + (size * 4));
 	return (this);
 }
 
-uint new_part(partBuffer *this)
+static uint newPart(partBuffer *this)
 {
-	for (uint id = this->size - 1; id != 0; id -= 1)
-		if (this->info[id].life <= 0)
-			return (id);
+	for (uint id = this ->size - 1; id != 0; id  -= 1)
+		if (this ->info[id].life  <= 0)
+			return  (id);
 	return ((uint)(-1));
 }
 
-int particules_init(int type)
+int init(int type,game_t *game)
 {
-	uint nb_particules = 1000;
+	int nb_particule = 1000;
+	//int w = 500;
+	//int h = 500;
 
-	if (window == NULL)
-		return (0);
-	sfRenderWindow_setFramerateLimit(window, 60);
-	buffer = new_part_buffer(nb_particules);
-	if (buffer == NULL)
-		return (0);
-	for (uint i = 0; i < nb_particules; i += 1) {
+	//window = sfRenderWindow_create (( sfVideoMode){w, h, 32},
+	//                            "Test", sfResize | sfClose , NULL);
+	//if (window == NULL)
+	//	return (0);
+	//sfRenderWindow_setFramerateLimit(window , 120);
+	buffer = newPartBuffer (nb_particule);
+	if (buffer  == NULL)
+		return  (0);
+	for (uint i = 0; i < nb_particule; i += 1) {
 		if (type == 1)
-			set_part_v1(buffer, i, (sfVector2f)
-			{250 + (rand() %150 - 75), 250 + (rand() % 150 - 75)});
+			setPartv1(buffer , i,(sfVector2f)
+			{250-1000+(rand() %150-75), 250+(rand() % 150 -75)});
 		if (type == 2)
-			set_part_v2(buffer, i, (sfVector2f){250 + (rand()
-			% 150 - 75), 250 + (rand() % 150 - 75)},
-			(sfColor){rand() % 255, rand()% 255,
-			rand() % 255, 255});
+			setPartv2(buffer , i, (sfVector2f){250+(rand()
+			% 150-75),250+(rand() % 150 -75)},(sfColor){rand()
+			% 255,rand()% 255, rand() % 255, 255});
 	}
 	return (1);
 }
 
-void particules(int type)
+void particule (int type,game_t *game)
 {
-	uint id;
-
-	update_part_buffer(buffer);
-	for (int i = 0; i < 10; i += 1) {
-		id = new_part(buffer);
+	updatePartBuffer(buffer);
+	for (int i = 0; i < 10; i += 1)
+	{
+		uint id = newPart(buffer);
 		if (id == (uint)(-1))
 			break;
-		if (type == 1)
-			set_part_v1(buffer, id, (sfVector2f){250, 250});
-		if (type == 2)
-			set_part_v2(buffer, id, (sfVector2f){250, 250},
-			(sfColor){rand() % 255, rand()
-			% 255, rand() % 255, 255});
+		if(type == 1)
+			setPartv1(buffer , id , (sfVector2f){800, 800});
+		if(type == 2)
+			setPartv2(buffer , id , (sfVector2f){250, 250},
+				(sfColor){rand() % 255, rand()
+				% 255, rand() % 255, 255});
 	}
-	sfRenderWindow_clear(window, sfBlack);
-	draw_part_buffer(buffer, window);
-	sfRenderWindow_display(window);
-}
-
-int particules_type(int type)
-{
-	if (particules_init(type) == 0)
-		return (1);
-	particules(type);
-	return (0);
+	//sfRenderWindow_clear(game->window->window , sfBlack);
+	drawPartBufer(buffer , game->window->window);
+	sfRenderWindow_display(game->window->window);
 }
